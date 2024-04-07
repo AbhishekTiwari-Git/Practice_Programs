8fb5e26e4dac64f9b6b305ebb34b047ef2560b00
Abhishek Tiwari - a0t0958 <Abhishek.Tiwari0@walmart.com>
2024-04-07 23:58:10 +0530

    new progs

--- src/main/java/org/example/Random.java
+++ src/main/java/org/example/Random.java
@@ -9,31 +9,21 @@ import static java.util.stream.Collectors.joining;
 
 public class Random {
     public static void main(String[] args) {
-        String original = "Title";
-        String replacement = "paper";
-        System.out.println(isIsoMorphic(original.toLowerCase(),replacement.toLowerCase()));
+        int[] nums = {2, 3, -2, -5, 6, -1, 4};
+        int leftProduct = 1;
+        int rightProduct = 1;
+        int n = nums.length;
+        int ans = nums[0];
 
-    }
+        for (int i = 0; i < n; i++) {
+            leftProduct = leftProduct == 0 ? 1 : leftProduct;
+            rightProduct = rightProduct == 0 ? 1 : rightProduct;
 
-    private static boolean isIsoMorphic(String orig, String repl) {
-        if(orig.length()!=repl.length()){
-            return false;
+            leftProduct = leftProduct * nums[i];
+            rightProduct = rightProduct * nums[n - 1 - i];
+
+            ans = Math.max(ans, Math.max(leftProduct, rightProduct));
         }
-        Map<Character,Character> map = new HashMap<>();
-        for(int i=0;i<orig.length()-1;i++){
-            if(!map.containsKey(orig.charAt(i))){
-                if(!map.containsValue(repl.charAt(i))){
-                    map.put(orig.charAt(i),repl.charAt(i));
-                }
-                else {
-                    return false;
-                }
-            }
-            else if(map.containsKey(orig.charAt(i))!= map.containsValue(repl.charAt(i))){
-                return false;
-            }
-        }
-        return true;
+        System.out.println(ans);
     }
-
 }
